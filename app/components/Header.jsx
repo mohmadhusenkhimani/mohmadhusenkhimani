e2c12f28cd51ddc5138418e17199adc8c135f560
Mohmadhusen Khimani <143099310+mohmadhusenkhimani@users.noreply.github.com>
2026-03-20 12:08:25 +0530

    Initial portfolio upload

---
+++ app/components/Header.jsx
@@ -0,0 +1,52 @@
+import { assets } from '@/assets/assets'
+import { motion } from "motion/react"
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
+      <motion.div
+      initial={{scale: 0}}
+      whileInView={{scale: 1}}
+      transition={{duration: 0.8, type: 'spring',stiffness: 100}}
+      >
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+      </motion.div>
+      <motion.h3 
+      initial={{y: -20, opacity: 0}}
+      whileInView={{y: 0, opacity:1}}
+      transition={{duration: 0.6,delay: 0.3}}
+      className='flex items-end gap-2 text-xl md:text-2xl mb-3'>Hi! I'm Mohmadhusen Khimani <Image src={assets.hand_icon} alt='' className='w-6'/> </motion.h3>
+      <motion.h1 
+      initial={{y: -30, opacity: 0}}
+      whileInView={{y: 0, opacity:1}}
+      transition={{duration: 0.8,delay: 0.5}}
+      className="text-4xl sm:text-6xl md:text-7xl font-bold leading-tight text-center tracking-tight">
+  Frontend Developer <br />
+  <span className="text-gray-600">React & Modern UI</span>
+</motion.h1>
+      <motion.p 
+      initial={{opacity: 0}}
+      whileInView={{opacity:1}}
+      transition={{duration: 0.6,delay: 0.7}}
+      className='max-w-2xl mx-auto font-Ovo'>Frontend Developer with hands-on experience in real projects, building responsive and interactive web applications.</motion.p>
+      <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+
+        <motion.a 
+      initial={{y: 30, opacity: 0}}
+      whileInView={{y: 0, opacity:1}}
+      transition={{duration: 0.6,delay: 1}}
+      href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></motion.a>
+
+        <motion.a 
+      initial={{y: 30, opacity: 0}}
+      whileInView={{y: 0, opacity:1}}
+      transition={{duration: 0.6,delay: 1.2}}
+      href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black'>my resume <Image src={assets.download_icon} alt='' className='w-4'/></motion.a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const { Outfit } = require('next/font/google');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F',
+      },
+      fontFamily: {
+        Outfit: ["Outfit","sans-serif"],
+        Ovo: ["Ovo","serif"]
+      },
+      boxShadow: {
+        'black' : '4px 4px 0 #000',
+        'white' : '4px 4px 0 #FFF',
+      },
+      gridTemplateColumns: {
+        'auto' : 'repeat(auto-fit, minmax(200px, 1fr))'
+      }
+    },
+  },
+  darkMode: 'selector',
+  plugins: [],
+};
